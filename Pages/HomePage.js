e1e4b80204898116413178728e6688407478540a
RyuHoSeok <dkflfkdeh@naver.com>
2017-06-21 10:24:48 +0900

    sidebar & init event list page

--- Pages/HomePage.js
+++ Pages/HomePage.js
@@ -3,13 +3,18 @@ var ImageControl = require("Modules/ImageControl");
 var Context = require("Modules/Context");
 
 var confirm_visible = Observable(false);
+var confirm_not_visible = Observable(true);
 
 var arg_save = null;
 
-function goToHistory(arg){
+function goToEditHistory(arg){
 	var history = arg.data;
 	// console.log(history);
 	router.push("editHistory", history);
+}
+
+function goToEventList(){
+	router.push("eventList");
 }
 
 function addHistory(){
@@ -20,10 +25,12 @@ function addHistory(){
 function confirm_visualization(arg){
 	arg_save = arg;
 	confirm_visible.value = true;
+	confirm_not_visible.value = false;
 }
 
 function confirm_hidden(){
 	confirm_visible.value = false;
+	confirm_not_visible.value = true;
 }
 
 function deleteHistory(){
@@ -36,8 +43,10 @@ function deleteHistory(){
 module.exports={
 	histories:Context.histories,
 	addHistory:addHistory,
-	goToHistory:goToHistory,
+	goToEditHistory:goToEditHistory,
+	goToEventList:goToEventList,
 	deleteHistory:deleteHistory,
+	confirm_not_visible:confirm_not_visible,
 	confirm_visible:confirm_visible,
 	confirm_visualization:confirm_visualization,
 	confirm_hidden:confirm_hidden,
